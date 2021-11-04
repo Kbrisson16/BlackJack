a7607d026e67980ba316993f037668bf96d3a512
KBrisson16 <KyleBrisson16@gmail.com>
2021-11-04 11:32:34 -0400

    Update ejs forms
    Added deleteAccount Page and Signup Page

--- Javascript/main.js
+++ Javascript/main.js
@@ -1,4 +1,4 @@
-window.onload = function() {
+window.onload = function () {
     document.getElementById("ChangePasswordButton").onclick = function () {
         location.href = "ChangePassword.ejs";
     }
